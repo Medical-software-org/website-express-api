3fa5588391efe0e87e0626bce0bc77a70ed94ac4
Vuyo Ncume <vuyo.ncume@vcontractor.co.za>
2023-05-16 19:43:33 +0200

    feature: base express app setup

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!",
+  });
+});
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/api`, router);
+
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
