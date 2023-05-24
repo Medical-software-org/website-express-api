0f323354bc39c5c9eaffce3cbfc3c2e6d5e072a3
Vuyo Ncume <vuyo.ncume@vcontractor.co.za>
2023-05-24 20:06:02 +0200

    feature: post request to firebase

--- src/services/email-list/email-list-routes.js
+++ src/services/email-list/email-list-routes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { emailListSignUp } = require("./validation-shemas");
+const admin = require("../../firebase-config");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
     const validatedData = await emailListSignUp.validate(req.body);
-    res.json(validatedData);
+    const ref = admin.database().ref("email-list").push();
+    await ref.set(validatedData);
+    res.status(201).json({ result: "success", payload: validatedData });
   } catch (error) {
+    console.log(error);
     res.status(400).json({ error: error.message });
   }
 });
